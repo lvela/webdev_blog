0c0c99dabae2dbd8d92f2014fadfd2ddd06222ea
Louis Vela <louis.vela@gmail.com>
2017-03-21 14:20:10 -0500

    Create comments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321185205) do
+ActiveRecord::Schema.define(version: 20170321191946) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 20170321185205) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
-    t.integer  "user_id"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
